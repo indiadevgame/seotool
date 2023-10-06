b52420ef3ec474dfeeac3d8f85a8f95d4ba11c73
Manytools <140259749+indiadevgame@users.noreply.github.com>
2023-10-06 06:27:44 -0600

    Add files via upload

---
+++ seo_websote_01/script.js
@@ -0,0 +1,8 @@
+let menu_bar = document.querySelector(".menu_bar");
+let menu_container = document.querySelector(".menu_container");
+
+menu_bar.addEventListener("click",()=>{
+    menu_container.classList.toggle("activate")
+})
+
+
